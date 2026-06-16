122749823ef7bacc65bdd2745c5e3d9193ae301d
Nytorgsstråket <hej@nytorgsstraket.se>
2026-06-16 19:51:02 +0200

    feat: add Sanity Studio deployment config with appId

--- nytorgsstraket-cms/sanity.cli.ts
+++ nytorgsstraket-cms/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'pw80xy2ch6lbzkqkk6rc67pg',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
